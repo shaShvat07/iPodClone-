8096629374eb86cf44dd98e5d77512887c8965d2
Shashvat <shashvatpatel2803@gmail.com>
2023-07-07 23:08:17 +0530

    Updates

---
+++ precache-manifest.521a9188237f4f57beb3dbd78ec77e9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cdffb9f2fa3feff53c45eb6fda1583",
+    "url": "/iPodClone-/index.html"
+  },
+  {
+    "revision": "843995506c6ff15a9b06",
+    "url": "/iPodClone-/static/css/main.087e9ec3.chunk.css"
+  },
+  {
+    "revision": "5e1c0844111721441d32",
+    "url": "/iPodClone-/static/js/2.87501b5f.chunk.js"
+  },
+  {
+    "revision": "e60eb46c359a399fe3462a1a90718ad7",
+    "url": "/iPodClone-/static/js/2.87501b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843995506c6ff15a9b06",
+    "url": "/iPodClone-/static/js/main.f794b659.chunk.js"
+  },
+  {
+    "revision": "5f2463719574ad2e8d9c",
+    "url": "/iPodClone-/static/js/runtime-main.d489254d.js"
+  },
+  {
+    "revision": "b3d72ad56aa7068161b6282b55a335f6",
+    "url": "/iPodClone-/static/media/3624579.b3d72ad5.jpg"
+  },
+  {
+    "revision": "f1b639b7ad9b98ba40e58802d5011377",
+    "url": "/iPodClone-/static/media/background.f1b639b7.jpg"
+  }
+]);
